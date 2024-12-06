10db635251ec3d2d540606625e5d344e3159532f
FrazierMark <m.mark.frazier@gmail.com>
2024-12-05 16:01:57 -0800

    background component implementation

--- canvas-component.js
+++ canvas-component.js
@@ -1,54 +1,101 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+import vertexShader from './shaders/background/vertexShader.js';
+import fragmentShader from './shaders/background/fragmentShader.js';
 
-class CanvasComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
+class CanvasBackground extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
 
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, 800 / 400, 0.1, 1000);
-    this.renderer = new THREE.WebGLRenderer({ alpha: true });
-    this.clock = new THREE.Clock();
-    this.orbitControls = new OrbitControls(
-      this.camera,
-      this.renderer.domElement
-    );
+    // Default negative z-index
+    const style = document.createElement('style');
+        style.textContent = `
+            :host {
+                position: fixed;
+                z-index: -1;
+                top: 0;
+                left: 0;
+            }
+        `;
+        this.shadowRoot.appendChild(style);
 
-    // Set renderer clear color to match body
-    this.renderer.setClearColor(0xffffff, 1);
+		this.sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		};
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera(
+			75,
+			this.sizes.width / this.sizes.height,
+			0.1,
+			1000
+		);
+		this.renderer = new THREE.WebGLRenderer({ alpha: true });
+		this.clock = new THREE.Clock();
+		this.orbitControls = new OrbitControls(
+			this.camera,
+			this.renderer.domElement
+		);
 
-    // Bind the tick method to the class instance
-    this.animate = this.animate.bind(this);
-  }
+		this.orbitControls.enableDamping = true;
+		this.renderer.setClearColor(0xffffff, 1);
+		this.animate = this.animate.bind(this);
+		this.backgroundMaterial = null;
+	}
 
-  /**
-   * Animate Function
-   */
-  animate() {
-    const elapsedTime = this.clock.getElapsedTime();
-    this.orbitControls.update();
-    this.renderer.render(this.scene, this.camera);
-    window.requestAnimationFrame(this.animate);
-  }
+	/**
+	 * Animate / Update
+	 */
+	animate() {
+		const elapsedTime = this.clock.getElapsedTime();
+		this.backgroundMaterial.uniforms.u_time.value = elapsedTime;
+		this.orbitControls.update();
+		this.renderer.render(this.scene, this.camera);
+		window.requestAnimationFrame(this.animate);
+	}
 
-  connectedCallback() {
-    this.renderer.setSize(
-      this.getAttribute('width') || 300,
-      this.getAttribute('height') || 150
-    );
+	connectedCallback() {
+		this.renderer.setSize(this.sizes.width, this.sizes.height);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    this.shadowRoot.appendChild(this.renderer.domElement);
+		this.shadowRoot.appendChild(this.renderer.domElement);
+		this.camera.position.set(0, 0.45, 0);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
 
-    this.camera.position.z = 5;
+		//Plane Geometry
+		const backgroundGeometry = new THREE.PlaneGeometry(2, 2, 256, 256);
 
-    this.animate();
-  }
+		// Plane Material
+		const colorA = this.getAttribute('color_a');
+		const colorB = this.getAttribute('color_b');
+
+    // Plane Material
+		this.backgroundMaterial = new THREE.ShaderMaterial({
+			vertexShader: vertexShader,
+			fragmentShader: fragmentShader,
+			uniforms: {
+				u_time: { value: 0 },
+				u_colorA: { value: new THREE.Color(colorA) },
+				u_colorB: { value: new THREE.Color(colorB) },
+				u_noiseStrength: { value: 0.13 },
+				u_noiseDensity: { value: 2.9 },
+			},
+		});
+
+		// Add Mesh, background = new THREE.Mesh(planeGeo, planeMaterial)
+		const background = new THREE.Mesh(
+			backgroundGeometry,
+			this.backgroundMaterial
+		);
+		background.position.set(0, 0, 0);
+		background.rotation.set(-Math.PI / 2, 0, 0);
+		background.scale.set(1.5, 1.5, 1.5);
+
+		this.scene.add(background);
+
+		this.animate();
+	}
 }
 
-customElements.define('canvas-component', CanvasComponent);
+customElements.define('canvas-background', CanvasBackground);
